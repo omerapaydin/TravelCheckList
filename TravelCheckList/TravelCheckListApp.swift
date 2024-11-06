7e241f598a157e775acb4a02ceddbb80dc8e8fb0
Ömer Apaydın <omerapaydin90@gmail.com>
2024-11-06 22:40:04 +0300

    Initial Commit

---
+++ TravelCheckList/TravelCheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCheckListApp.swift
+//  TravelCheckList
+//
+//  Created by Ömer on 6.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelCheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelCheckListTests/TravelCheckListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCheckListTests.swift
+//  TravelCheckListTests
+//
+//  Created by Ömer on 6.11.2024.
+//
+
+import Testing
+@testable import TravelCheckList
+
+struct TravelCheckListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
